f51b0bf1ea15bb7d000dabeab5c36e7662598c06
Brad McCollum <btmcco8@gmail.com>
2018-08-11 22:01:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
 def reverse_each_word(string)
   sentence = string.split
-  response = sentence.each do |word|
-     puts"#{word.reverse}"
-   end
-     response.join(" ")
 end
 
 # def reverse_each_word(string)
