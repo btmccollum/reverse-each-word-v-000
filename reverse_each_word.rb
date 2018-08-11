943d9682d3e87f553e2e441cf079c47ba20f9536
Brad McCollum <btmcco8@gmail.com>
2018-08-11 22:07:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(string) #reverses each word in the sentence in place using
   sentence.each{|word| response << word.reverse}.join(" ")
 end
 
-def reverse_each_word(string) #reverses each word in the sentence in place using '.collect'
-  sentence = string.split
-  response = sentence.collect{|word| word.reverse}.join(" ")
-  # response.join(" ")
-end
+# def reverse_each_word(string) #reverses each word in the sentence in place using '.collect'
+#   sentence = string.split
+#   response = sentence.collect{|word| word.reverse}.join(" ")
+#   # response.join(" ")
+# end
