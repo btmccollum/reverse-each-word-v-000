dc54e05170512ce5614f3393413f432975c62ae0
Brad McCollum <btmcco8@gmail.com>
2018-08-11 22:02:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
   sentence = string.split
   sentence.each do |word|
-    word.reverse
+    print word.reverse
   end
- sentence
+ 
 end
 
 # def reverse_each_word(string)
